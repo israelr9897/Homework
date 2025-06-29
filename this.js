71e84d5cf79394fdcccb8d24ce814a4c4a199e34
israelr9897 <israelr9897@gmail.com>
2025-06-29 18:23:23 +0300

    finish 3

--- this.js
+++ this.js
@@ -2,9 +2,11 @@
 function User(name){
     this.name = name;
 }
+
 User.prototype.sayName = function(){
     console.log("User: " + this.name);
 }
+
 const user1 = new User("avi");
 const user2 = new User("ari");
 user1.sayName();
@@ -12,8 +14,9 @@ user2.sayName();
 
 //2
 const obj = {
+    name : "israel",
     sayNewName : () => {
-        console.log(this.name);
+        console.log(this);
     },
     sayNewName2 : function(){
         console.log(this.name);
@@ -21,4 +24,16 @@ const obj = {
 }
 obj.sayNewName();
 obj.sayNewName2();
+
+//3
+const obj2 = {
+    log : function(){
+        console.log("Welcome," + this); 
+    }
+}
+function logger(fn){
+    fn();
+}
+
+logger(() => obj2.log());
 
