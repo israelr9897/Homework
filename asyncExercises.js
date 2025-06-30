de615e90e8cd66602476bbe0f478906fecc5169a
israelr9897 <israelr9897@gmail.com>
2025-06-30 15:41:06 +0300

    finish exercis of async

--- asyncExercises.js
+++ asyncExercises.js
@@ -1,5 +1,6 @@
 import { getRandomValues } from "crypto";
 import fs from "fs";
+import os from "os";
 
 //1
 // fs.readFile("./text.txt", "utf-8", (err, data) => {
@@ -51,7 +52,17 @@ import fs from "fs";
 //   console.log(lines[Math.floor(Math.random() * lines.length)]);
 // });
 
-// //5
-// setInterval(() => {
-//     console.log();
-// }, 2);
+//5
+let counter = 0;
+function printMemory() {
+  counter++;
+  const totalMemory = os.totalmem();
+  const freeMemory = os.freemem();
+  const memory = 100 - ((freeMemory / totalMemory) * 100).toFixed(2);
+  console.log(`Free Memory: ${memory}%`);
+  if (counter >= 5) {
+    clearInterval(playInterval);
+  }
+}
+
+const playInterval = setInterval(printMemory, 1000);
